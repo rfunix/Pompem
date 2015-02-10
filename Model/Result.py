72c93865022301bd3c264ea5124ea2da60c58877
Will Filho <willxc2@gmail.com>
2015-02-10 10:19:06 -0200

    delete Model folder

--- Model/Result.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-import sys
-sys.path.insert(0, '..')
-
-class Result:
-    def __init__(self, date=None, description=None, author=None):
-        self.date = date
-        self.description = description
-        self.author = author
